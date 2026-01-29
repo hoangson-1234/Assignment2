736f8cf15b6933419782ab24957d136da7bb7320
hoangson-1234 <danghoangson2752k4@gmail.com>
2026-01-29 11:15:00 +0700

    Implement Multiply Calculator

---
+++ Multiple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter number 2: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double result = a * b;
+
+            Console.WriteLine("Result = " + result);
+            Console.ReadLine();
+        }
+    }
+}
