37348cede0275ac5339260115cd3ab602459826b
hoangson-1234 <danghoangson2752k4@gmail.com>
2026-01-29 11:25:03 +0700

    Implement Devide Calculator

---
+++ Devide/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Divide
+    {
+        public static double Calculator(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+
+            return a / b;
+        }
+    }
+}
