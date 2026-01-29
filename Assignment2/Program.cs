72765bebe455d4a281797920b6e6530cd712b649
Hanh-HE180572 <nghoanganh218@gmail.com>
2026-01-29 13:27:10 +0700

    Implement plus operation

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -2,35 +2,18 @@
 {
     using System;
 
-    enum SIGN
-    {
-        PLUS,
-        MINUS,
-        MULTIPLY,
-        DIVIDE
-    }
-
     class Program
     {
         static void Main()
         {
             Console.Write("Enter a: ");
-            double a = double.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
 
             Console.Write("Enter b: ");
-            double b = double.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
 
-            double result = calculate(SIGN.PLUS, a, b);
+            int result = a + b;
             Console.WriteLine("Result = " + result);
-        }
-
-        static double calculate(SIGN sign, double arg1, double arg2)
-        {
-            switch (sign)
-            {
-                default:
-                    return 0;
-            }
         }
     }
 }
