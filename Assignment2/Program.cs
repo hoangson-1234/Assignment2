bec00064e278c42e1adfff78918e1bcd7ce0782d
Hanh-HE180572 <nghoanganh218@gmail.com>
2026-01-29 13:23:15 +0700

    Initial commit - base project

---
+++ Assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment2
+{
+    using System;
+
+    enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double result = calculate(SIGN.PLUS, a, b);
+            Console.WriteLine("Result = " + result);
+        }
+
+        static double calculate(SIGN sign, double arg1, double arg2)
+        {
+            switch (sign)
+            {
+                default:
+                    return 0;
+            }
+        }
+    }
+}
