bbc12ce25b63430f70d3767ee1d21aaf0efcf899
Hanh-HE180572 <nghoanganh218@gmail.com>
2026-01-29 11:20:11 +0700

    Initial commit - Visual Studio project

---
+++ Assignment2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment2
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a + b;
+            Console.WriteLine("Result = " + result);
+        }
+    }
+}
