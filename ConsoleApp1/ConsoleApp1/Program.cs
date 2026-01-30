06742cd729063c9ddd9b67d9e564ad34b044bdbf
duongyt2207-gif <duongyt2207@gmail.com>
2026-01-30 14:30:20 +0700

    Fix

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SubtractionApp
+    {
+        class Program
+        {
+            public static int Subtract(int x, int y)
+            {
+                return x - y;
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("=== SUBTRACTION ===");
+                Console.Write("Enter number a: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number b: ");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Result: {a} - {b} = {Subtract(a, b)}");
+            }
+        }
+    }
