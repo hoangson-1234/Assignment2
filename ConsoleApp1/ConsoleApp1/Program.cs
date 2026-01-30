0522d806c466cff9d6e1e360d22e3d56f97deb68
duongyt2207-gif <duongyt2207@gmail.com>
2026-01-30 14:49:27 +0700

    Implement multiply operation

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationApp
+{
+    class Program
+    {
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== MULTIPLICATION ===");
+            Console.Write("Enter number a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result: {a} * {b} = {Multiply(a, b)}");
+        }
+    }
+}
