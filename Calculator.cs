893e76daf73a1b99f2617ff33a05ca0c57d18e21
ducddhe180839 <dangduc141119@gmail.com>
2026-01-31 17:04:30 +0700

    Initial base code for calculator

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+   
+    public enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    public class Calculator
+    {
+        
+        private Double calculate(SIGN sign, Double arg1, Double arg2)
+        {
+            switch (sign)
+            {
+                default:
+                    break;
+            }
+            return 0d; 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,5 +6,10 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        
     }
+
 }
+
+
